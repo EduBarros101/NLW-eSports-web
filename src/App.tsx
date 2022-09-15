fe859ca963d1f17b4272b57202789c55dd21dc89
Eduardo Barros <edu.barros101@gmail.com>
2022-09-15 11:20:46 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/main.css'
+
+import logoImg from './assets/logo-nlw-esports.svg'
+
+export function App() {
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center'>
+      <img src={logoImg} alt='' />
+    </div>
+  )
+}
